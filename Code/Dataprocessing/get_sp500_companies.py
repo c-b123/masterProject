89935447136f1f167850cbe7cd9296e56c8b7cc1
Christian <christian.berger2@uzh.ch>
2023-07-08 16:21:35 +0200

    file with sp500 companies added

---
+++ Code/Dataprocessing/get_sp500_companies.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+table = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+df = table[0]
+# df.to_csv(r"C:\Users\chris\IdeaProjects\masterProject\Dataset\sp500.csv")
