69ce9c43d03928502e0439cd2b6af6bdc80aeade
Christian <christian.berger2@uzh.ch>
2023-07-10 11:04:08 +0200

    stockData.py: renaming and documentation added

--- Code/Dataprocessing/stockData.py
+++ Code/Dataprocessing/stockData.py
@@ -2,15 +2,28 @@ import yfinance as yf
 import resources as r
 
 
-# give dataframe with news articles get stocks
-# using a window size of 7 
-# use the most common 50 companies 
-# keep track of dates and companies for which we have stock prices
-
 def get_stock_data(ticker, start_date, end_date):
-    stock_data = yf.download(ticker, start=start_date, end=end_date)
-    return stock_data
+    """
+    Returns a dataframe with date, open, high, low, close, adjusted close, and volume for a single stock.
 
+    Parameters
+    ----------
+    ticker : str
+        The company ticker.
+    start_date : str
+        The start date in the format: %y-%m-%d.
+    end_date : str
+        The end date in the format: %y-%m-%d.
+
+    Returns
+    -------
+    object
+        dataframe with date, open, high, low, close, adjusted close, and volume as columns
+    """
+
+    stock_data = yf.download(ticker, start=start_date, end=end_date)
+
+    return stock_data
 
 # company_ticker = "AAPL"  # Apple Inc. stock ticker
 # company_name = "Apple"  # Company name to search news
