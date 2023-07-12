05485ff1b57adba0c075ca0826f00f5b80d48f21
Christian <christian.berger2@uzh.ch>
2023-07-12 10:16:39 +0200

    sp500_constituents.csv: renaming sp500_data.csv: csv containing the price and return information for the S&P500

--- Code/Dataprocessing/get_sp500_companies.py
+++ Code/Dataprocessing/get_sp500_companies.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 table = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
 df = table[0]
-# df.to_csv(r"C:\Users\chris\IdeaProjects\masterProject\Dataset\sp500.csv")
+# df.to_csv(r"C:\Users\chris\IdeaProjects\masterProject\Dataset\sp500_constituents.csv")
