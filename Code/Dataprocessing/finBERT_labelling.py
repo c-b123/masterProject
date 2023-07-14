bff7952f2d1a471efe65bd1df808d45f5bf24d4f
Christian <christian.berger2@uzh.ch>
2023-07-14 10:10:13 +0200

    finBERT_labelling.py: labelling of text data uses GPU acceleration now

--- Code/Dataprocessing/finBERT_labelling.py
+++ Code/Dataprocessing/finBERT_labelling.py
@@ -1,28 +1,49 @@
-from datasets import load_dataset
 from transformers import pipeline
+from datasets import load_dataset
+from torch.utils.data import Dataset
+from tqdm.auto import tqdm
 
-# Read dataset
-d = load_dataset("csv", data_files="C:/Users/chris/IdeaProjects/masterProject/Dataset/ar_price.csv")
-
-# Initialize the pipeline with finBERT
-pipe = pipeline(model="ProsusAI/finbert", tokenizer='ProsusAI/finbert')
-
-
-# Define labelling function which gets mapped to the dataset
-def label(record):
-    record["finBERT"] = pipe(record['title'])[0]['label']
-
-    return record
+########################################################################################################################
+# This script needs GPU acceleration e.g. via Google Colab
+########################################################################################################################
 
 
-# Select a portion of the dataset using slicing
-portion = d["train"].select(range(0, 20))
+# Define dataset structure
+class ArDataset(Dataset):
+    def __init__(self, original_list):
+        self.original_list = original_list
 
-# Map the defined function to the entire dataset
-portion = portion.map(label)
+    def __len__(self):
+        return len(self.original_list)
 
-# Convert the dataset to a pandas DataFrame
-df = portion.to_pandas()
+    def __getitem__(self, i):
+        return self.original_list[i]
+
+
+# Load dataset
+d = load_dataset("csv", data_files="/content/drive/MyDrive/masterProject/analyst_ratings_with_price.csv")
+d = d["train"]
+
+# Instantiate dataset
+dataset = ArDataset(d["title"])
+
+# Find optimal batch size on subset of data
+# for batch_size in [1, 8, 64, 256]:
+#     print("-" * 30)
+#     print(f"Streaming batch_size={batch_size}")
+#     for out in tqdm(pipe(dataset, batch_size=batch_size), total=len(dataset)):
+#         pass
+
+# Create a pipeline using the desired model and tokenizer using a GPU (device=0)
+pipe = pipeline(model="ProsusAI/finbert", tokenizer='ProsusAI/finbert', device=0)
+
+# Label text data
+results = []
+for out in tqdm(pipe(dataset, batch_size=8), total=len(dataset)):
+    results.append(out["label"])
+
+# Add results to the original dataset
+d = d.add_column("finBERT", results)
 
 # Save the DataFrame as a CSV file
-df.to_csv("path/to/save/dataset.csv", index=False)
+# d.to_csv("/content/drive/MyDrive/masterProject/analyst_ratings_labelled.csv", index=False)
