deecda2e173bc0cfaa8df3e5ad7da05ef0d73662
Christian <christian.berger2@uzh.ch>
2023-07-14 10:26:23 +0200

    comments added and minor refactoring

--- Code/Dataprocessing/join_stock_and_market.py
+++ Code/Dataprocessing/join_stock_and_market.py
@@ -1,14 +1,19 @@
 import pandas as pd
 
-analyst_ratings_labelled = pd.read_csv(
-    r"/Dataset/ar_labelled.csv", index_col=0)
+# Read ar_labelled, containing stock data and finBERT labels
+ar_labelled = pd.read_csv(r"C:\Users\chris\IdeaProjects\masterProject\Dataset\ar_labelled.csv", index_col=0)
+
+# Read sp500_kpis, containing market data
 sp500_kpis = pd.read_csv(r"C:\Users\chris\IdeaProjects\masterProject\Dataset\sp500_kpis.csv")
 
-analyst_ratings_labelled = pd.merge(analyst_ratings_labelled, sp500_kpis, how="left", on="date")
+# Merge both datasets on date
+ar_labelled_market = pd.merge(ar_labelled, sp500_kpis, how="left", on="date")
 
-analyst_ratings_labelled.rename(columns={'mean': "sp_mean", 'variance': "sp_var", '10th_percentile': "sp_10_pct",
-                                         '25th_percentile': "sp_25_pct", '50th_percentile': "sp_50_pct",
-                                         '75th_percentile': "sp_75_pct", '90th_percentile': "sp_90_pct"},
-                                errors="raise", inplace=True)
+# Rename the column names for better understanding
+ar_labelled_market.rename(columns={'mean': "sp_mean", 'variance': "sp_var", '10th_percentile': "sp_10_pct",
+                                   '25th_percentile': "sp_25_pct", '50th_percentile': "sp_50_pct",
+                                   '75th_percentile': "sp_75_pct", '90th_percentile': "sp_90_pct"},
+                          errors="raise", inplace=True)
 
-analyst_ratings_labelled.to_csv(r"C:\Users\chris\IdeaProjects\masterProject\Dataset\ar_labelled_market.csv")
+# Export merged dataset as csv
+ar_labelled_market.to_csv(r"C:\Users\chris\IdeaProjects\masterProject\Dataset\ar_labelled_market.csv")
