586a08de2841d4876af145c4eb390377b911d5b2
Christian <christian.berger2@uzh.ch>
2023-07-15 15:55:48 +0200

    several improvements on logistic regression, some refactoring

--- Code/Standard ML/logistic-regression.py
+++ Code/Standard ML/logistic-regression.py
@@ -1,10 +1,12 @@
-import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import RepeatedStratifiedKFold, cross_val_score, train_test_split
+import pandas as pd
+from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import classification_report
+from Code.Dataprocessing import df_processing as dp
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
+from sklearn.model_selection import RepeatedStratifiedKFold, cross_val_score, train_test_split
 
 # https://machinelearningmastery.com/multinomial-logistic-regression-with-python/
 # https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html
@@ -13,41 +15,39 @@ from sklearn.metrics import classification_report
 file = r"C:\Users\chris\IdeaProjects\masterProject\Dataset\ar_labelled_market.csv"
 df = pd.read_csv(file, index_col=0)
 
+
+########################################################################################################################
+# Data preprocessing
+########################################################################################################################
+
 # Convert finBERT labels into categorical variable
 df["finBERT"] = pd.Categorical(df["finBERT"], ordered=True, categories=["negative", "neutral", "positive"])
 
+# Add relative return
+df = dp.add_relative_return(df, 'sp_25_pct', 'sp_75_pct')
+
 # Only take data for only specific stock
-# print(df["stock"].value_counts())
-# df = df.loc[df["stock"] == "MRK"]
-
-
-########################################################################################################################
-# Balance dataset and define independent variables
-########################################################################################################################
-
-
-def balance_dataframe(df, target_column):
-    class_counts = df[target_column].value_counts()
-    min_class_count = class_counts.min()
-    balanced_df = df.groupby(target_column).apply(lambda x: x.sample(min_class_count, random_state=42))
-    balanced_df = balanced_df.sample(frac=1, random_state=42).reset_index(drop=True)
-    return balanced_df
-
+df = df.loc[df["stock"] == "MS"]
 
 # Apply balancing to dataset
-df = balance_dataframe(df, 'finBERT')
+df = dp.balance_dataframe(df, 'finBERT')
 
 # Show the distribution of positive, neutral and negative labels
 df["finBERT"].value_counts().plot(kind="bar")
 plt.show()
 
+
+########################################################################################################################
+# Define independent variables
+########################################################################################################################
+
 # Define independent and dependent variable
 independent1 = ['open', 'high', 'low', 'close', 'adj close', 'volume', 'return', 'log_return', 'sp_mean',
                 'sp_var', 'sp_10_pct', 'sp_25_pct', 'sp_50_pct', 'sp_75_pct', 'sp_90_pct']
 independent2 = ['return', 'sp_var', 'sp_25_pct', 'sp_75_pct']
-X = df.loc[:, df.columns.isin(independent2)].values
+independent3 = ['return', 'sp_var', "under", "neutral", "out"]
+X = df.loc[:, df.columns.isin(independent3)].values
 y = df.loc[:, df.columns == "finBERT"].values.ravel()
-
 
 ########################################################################################################################
 # Multinomial Logistic Regression
@@ -57,10 +57,21 @@ y = df.loc[:, df.columns == "finBERT"].values.ravel()
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, shuffle=True)
 
 # Standardize independent variable
-scaler = StandardScaler()
-scaler.fit(X_train)
-scaler.transform(X_train)
-scaler.transform(X_test)
+# scaler = StandardScaler()
+# scaler.fit(X_train)
+# scaler.transform(X_train)
+# scaler.transform(X_test)
+
+# Define the ColumnTransformer with the desired transformers, required when dummy variables in independent variables
+ct = ColumnTransformer(
+    transformers=[
+        ('standardization', StandardScaler(), [0])
+    ],
+    remainder='passthrough'  # Pass through any remaining columns
+)
+ct.fit(X_train)
+ct.transform(X_train)
+ct.transform(X_test)
 
 # Define Multinomial Logistic Regression model
 model = LogisticRegression(multi_class='multinomial', solver='lbfgs')
@@ -74,27 +85,33 @@ y_hat = model.predict(X_test)
 # Get classification report
 print(classification_report(y_test, y_hat, target_names=["negative", "neutral", "positive"]))
 
+# Get confusion matrix
+cm = confusion_matrix(y_test, y_hat, labels=["negative", "neutral", "positive"])
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_)
+disp.plot()
+plt.show()
+
 
 ########################################################################################################################
 # Multinomial Logistic Regression with KFold validation
 ########################################################################################################################
 
-# Standardize independent variable
-scaler = StandardScaler()
-X = scaler.fit_transform(X)
-
-# Define Multinomial Logistic Regression model
-model = LogisticRegression(multi_class='multinomial', solver='lbfgs')
-
-# Define the model evaluation procedure
-cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
-
-# Evaluate the model and collect the scores
-n_scores = cross_val_score(model, X, y, scoring='accuracy', cv=cv, n_jobs=-1)
-
-# Report performance
-print('Accuracy for Multinomial Logistic Regression:\n'
-      '{0:.3f} ({1:.3f})'.format(np.mean(n_scores), np.std(n_scores)))
+# # Standardize independent variable
+# scaler = StandardScaler()
+# X = scaler.fit_transform(X)
+#
+# # Define Multinomial Logistic Regression model
+# model = LogisticRegression(multi_class='multinomial', solver='lbfgs')
+#
+# # Define the model evaluation procedure
+# cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+#
+# # Evaluate the model and collect the scores
+# n_scores = cross_val_score(model, X, y, scoring='accuracy', cv=cv, n_jobs=-1)
+#
+# # Report performance
+# print('Accuracy for Multinomial Logistic Regression:\n'
+#       '{0:.3f} ({1:.3f})'.format(np.mean(n_scores), np.std(n_scores)))
 
 
 ########################################################################################################################
@@ -148,9 +165,10 @@ for name, model in models.items():
 
 # add more KPIs with Pyfolio
 # add S&P500 return as KPI -done
-# only positive and negative
 # balance dataset - done
 # try ordinal logistic regression
 # lag data
 # Standardizing/Normalize - done
 # use window of returns for independent variable
+# confusion matrix - done
+# outperforming, neutral, and underperforming return - done
