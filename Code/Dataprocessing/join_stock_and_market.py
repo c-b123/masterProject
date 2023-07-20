8343126622f158dbfa5bbb7b080d2d625822c260
Christian <christian.berger2@uzh.ch>
2023-07-20 17:06:58 +0200

    join_stock_and_market applied to new dataset

--- Code/Dataprocessing/join_stock_and_market.py
+++ Code/Dataprocessing/join_stock_and_market.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Read ar_labelled, containing stock data and finBERT labels
-ar_labelled = pd.read_csv(r"C:\Users\chris\IdeaProjects\masterProject\Dataset\ar_labelled.csv", index_col=0)
+ar_labelled = pd.read_csv(r"C:\Users\chris\IdeaProjects\masterProject\Dataset\av_labelled.csv", index_col=0)
 
 # Read sp500_kpis, containing market data
 sp500_kpis = pd.read_csv(r"C:\Users\chris\IdeaProjects\masterProject\Dataset\sp500_kpis.csv")
@@ -10,10 +10,15 @@ sp500_kpis = pd.read_csv(r"C:\Users\chris\IdeaProjects\masterProject\Dataset\sp5
 ar_labelled_market = pd.merge(ar_labelled, sp500_kpis, how="left", on="date")
 
 # Rename the column names for better understanding
-ar_labelled_market.rename(columns={'mean': "sp_mean", 'variance': "sp_var", '10th_percentile': "sp_10_pct",
-                                   '25th_percentile': "sp_25_pct", '50th_percentile': "sp_50_pct",
-                                   '75th_percentile': "sp_75_pct", '90th_percentile': "sp_90_pct"},
+ar_labelled_market.rename(columns={'mean': "sp_mean", 'variance': "sp_var",
+                                   '10th_percentile': "sp_10_pct",
+                                   '20th_percentile': "sp_20_pct",
+                                   '25th_percentile': "sp_25_pct",
+                                   '50th_percentile': "sp_50_pct",
+                                   '75th_percentile': "sp_75_pct",
+                                   '80th_percentile': "sp_80_pct",
+                                   '90th_percentile': "sp_90_pct"},
                           errors="raise", inplace=True)
 
 # Export merged dataset as csv
-ar_labelled_market.to_csv(r"C:\Users\chris\IdeaProjects\masterProject\Dataset\ar_labelled_market.csv")
+ar_labelled_market.to_csv(r"C:\Users\chris\IdeaProjects\masterProject\Dataset\av_labelled_market.csv")
