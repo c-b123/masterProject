d09d8945da564ee33ace259ce4be83eee8ffb397
Christian <christian.berger2@uzh.ch>
2023-07-20 16:37:22 +0200

    finBERT labelling applied to new dataset

--- Code/Dataprocessing/finBERT_labelling.py
+++ Code/Dataprocessing/finBERT_labelling.py
@@ -1,15 +1,18 @@
 from transformers import pipeline
-from datasets import load_dataset
 from torch.utils.data import Dataset
 from tqdm.auto import tqdm
+from datasets import load_dataset
 
 ########################################################################################################################
 # This script needs GPU acceleration e.g. via Google Colab
 ########################################################################################################################
 
+# Create a pipeline using the desired model and tokenizer using a GPU
+pipe = pipeline(model="ProsusAI/finbert", tokenizer='ProsusAI/finbert', device=0)
+
 
 # Define dataset structure
-class ArDataset(Dataset):
+class NewsDataset(Dataset):
     def __init__(self, original_list):
         self.original_list = original_list
 
@@ -21,21 +24,19 @@ class ArDataset(Dataset):
 
 
 # Load dataset
-d = load_dataset("csv", data_files="/content/drive/MyDrive/masterProject/analyst_ratings_with_price.csv")
+d = load_dataset("csv", data_files="/content/drive/MyDrive/masterProject/av_data_processed.csv")
 d = d["train"]
 
 # Instantiate dataset
-dataset = ArDataset(d["title"])
+dataset = NewsDataset(d["summary"])
 
-# Find optimal batch size on subset of data
+# # Find optimal batch size on subset of data
 # for batch_size in [1, 8, 64, 256]:
 #     print("-" * 30)
 #     print(f"Streaming batch_size={batch_size}")
 #     for out in tqdm(pipe(dataset, batch_size=batch_size), total=len(dataset)):
 #         pass
 
-# Create a pipeline using the desired model and tokenizer using a GPU (device=0)
-pipe = pipeline(model="ProsusAI/finbert", tokenizer='ProsusAI/finbert', device=0)
 
 # Label text data
 results = []
@@ -46,4 +47,4 @@ for out in tqdm(pipe(dataset, batch_size=8), total=len(dataset)):
 d = d.add_column("finBERT", results)
 
 # Save the DataFrame as a CSV file
-# d.to_csv("/content/drive/MyDrive/masterProject/analyst_ratings_labelled.csv", index=False)
+# d.to_csv("/content/drive/MyDrive/masterProject/av_data_labelled.csv", index=False)
