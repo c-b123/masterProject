cc594550e6b600e2cc47d492629b8c5444f4b4fe
Christian <christian.berger2@uzh.ch>
2023-07-24 20:21:16 +0200

    less restrictive ticker assignment, some reformatting

--- Code/Alpha Vantage/av_cleaning.py
+++ Code/Alpha Vantage/av_cleaning.py
@@ -1,9 +1,8 @@
 import ast
 from datetime import datetime
-
 import matplotlib.pyplot as plt
+import pandas
 import pandas as pd
-
 from Code import resources as r
 
 
@@ -16,7 +15,27 @@ def convert_date(date_string):
 def get_ticker_with_max_relevance(ticker_list):
     tickers = ast.literal_eval(ticker_list)
     max_relevance = max(tickers, key=lambda x: float(x['relevance_score']))
+
     return max_relevance["ticker"], float(max_relevance["relevance_score"])
+
+
+def get_relevant_ticker(ticker_list):
+    tickers = ast.literal_eval(ticker_list)
+    tickers = [ticker for ticker in tickers if ticker["ticker"] in r.tickers]
+    max_relevance = max(tickers, key=lambda x: float(x['relevance_score']))
+
+    return max_relevance["ticker"], float(max_relevance["relevance_score"])
+
+
+def balance_data(dataframe: pandas.DataFrame, column: str, n_samples: int):
+    grouped = dataframe.groupby(column)
+    selected_rows = []
+    for group_name, group_df in grouped:
+        if True:
+            selected_rows.append(group_df.sample(n=n_samples))
+    dataframe = pd.concat(selected_rows)
+
+    return dataframe
 
 
 # Load dataset
@@ -32,27 +51,20 @@ df.rename(columns={'time_published': 'date'}, inplace=True)
 df['date'] = df['date'].apply(lambda x: convert_date(x))
 
 # Assign ticker with max relevance to news
-df['stock'], df['relevance'] = zip(*df['ticker_sentiment'].apply(get_ticker_with_max_relevance))
+df['stock'], df['relevance'] = zip(*df['ticker_sentiment'].apply(get_relevant_ticker))
 
 # Drop all stocks which are not of interest
 df = df[df['stock'].isin(r.tickers)]
 
 # Only consider news with a relevance score higher than 0.2
-df = df.loc[df['relevance'] > 0.2]
+df = df.loc[df['relevance'] > 0.1]
 
 # Balance the number of news per stock
-grouped = df.groupby('stock')
-news_per_stock = 1252
-selected_rows = []
-for group_name, group_df in grouped:
-    if len(group_df) >= 1000:
-        selected_rows.append(group_df.sample(n=news_per_stock))
-    else:
-        selected_rows.append(group_df)
-df = pd.concat(selected_rows)
+df = balance_data(df, "stock", 625)
 
 # Print dataset length
 print(f"Dataset size: {len(df)}")
+print(f"Ebay records: {len(df[df['stock'] == 'EBAY'])}")
 
 # Histogram of stock distribution
 hist_sto = df["stock"].value_counts().plot(kind='bar')
@@ -63,9 +75,8 @@ plt.title('Stock News Distribution')
 plt.show()
 
 # Histogram of relevance distribution
-# df["relevance"].value_counts().plot(kind='bar')
-bin_rel = [0.2, 0.4, 0.6, 0.8, 1]
-bin_rel_labels = ['<0.4', '0.4-0.6', '0.6-0.8', '0.8-1']
+bin_rel = [0, 0.2, 0.4, 0.6, 0.8, 1]
+bin_rel_labels = ['<0.2', '0.2-0.4', '0.4-0.6', '0.6-0.8', '0.8-1']
 hist_rel = pd.cut(df['relevance'], bins=bin_rel, labels=bin_rel_labels, right=False).value_counts().sort_index().plot(
     kind='bar')
 hist_rel.set_xticklabels(hist_rel.get_xticklabels(), rotation=0)
@@ -75,6 +86,6 @@ plt.ylabel('Count')
 plt.title('Relevance Score Distribution')
 plt.show()
 
-# Save as csv file
-df.to_csv(r"C:\Users\chris\IdeaProjects\masterProject\Dataset\av_clean.csv", index=False,
-          columns=["title", "date", "summary", "stock", "relevance"])
+# # Save as csv file
+# df.to_csv(r"C:\Users\chris\IdeaProjects\masterProject\Dataset\av_clean.csv", index=False,
+#           columns=["title", "date", "summary", "stock", "relevance"])
