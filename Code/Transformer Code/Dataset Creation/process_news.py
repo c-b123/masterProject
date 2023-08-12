9624ceb386f477591e63c618d31094fdbb47b58d
MuHamza98 <49498843+MuHamza98@users.noreply.github.com>
2023-08-12 17:46:33 +0200

    Update process_news.py

--- Code/Transformer Code/Dataset Creation/process_news.py
+++ Code/Transformer Code/Dataset Creation/process_news.py
@@ -14,17 +14,17 @@ company_tickers = [
     'FYBR', 'LUV', 'KLM', 'JBLU', 'LHA.DE', 'NVS', 'ABT', 'MDTKF', 'AMGN'
     ]
 company_similarity_mapping = {
-    # 'MRK':['PFE', 'GSK', 'SNY', 'MRK.DE'],
-    # 'MS':['GS', 'JPM', 'UBS', 'WFC'],
-    # 'MU':['000660.KS', 'INTC', 'WDC', 'NVDA'],
-    # 'NVDA':['AMD', 'MSFT', '2357.TW', 'INTC'],
-    # 'EBAY':['AMZN', 'WMT', 'TGT', 'HM-B.ST'],
-    # 'NFLX':['DIS', 'AAPL', 'MSFT', 'AMZN'],
-    # 'QQQ':['DJIA', 'SPX', 'IXIC', 'RUT'],
-    # 'VZ' : ['T', 'USM', 'LUMN', 'FYBR'],
-    # 'DAL':['LUV', 'KLM', 'JBLU', 'LHA.DE'],
-    # 'JNJ':['NVS', 'ABT', 'PFE', 'GSK'],
-    # 'QCOM':['MDTKF', 'NVDA', 'INTC', 'AMD'],
+    'MRK':['PFE', 'GSK', 'SNY', 'MRK.DE'],
+    'MS':['GS', 'JPM', 'UBS', 'WFC'],
+    'MU':['000660.KS', 'INTC', 'WDC', 'NVDA'],
+    'NVDA':['AMD', 'MSFT', '2357.TW', 'INTC'],
+    'EBAY':['AMZN', 'WMT', 'TGT', 'HM-B.ST'],
+    'NFLX':['DIS', 'AAPL', 'MSFT', 'AMZN'],
+    'QQQ':['DJIA', 'SPX', 'IXIC', 'RUT'],
+    'VZ' : ['T', 'USM', 'LUMN', 'FYBR'],
+    'DAL':['LUV', 'KLM', 'JBLU', 'LHA.DE'],
+    'JNJ':['NVS', 'ABT', 'PFE', 'GSK'],
+    'QCOM':['MDTKF', 'NVDA', 'INTC', 'AMD'],
     'AMGN':['JNJ', 'ABT', 'PFE', 'GSK'],
 }
 
